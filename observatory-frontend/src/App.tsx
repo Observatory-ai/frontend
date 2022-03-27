c9a2cedbf315d787c040751ba807f69dab3b14ca
Hrithik Shah <hrithikchamp@gmail.com>
2022-03-27 00:55:54 -0400

    added header and chart

--- observatory-frontend/src/App.tsx
+++ observatory-frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import * as charts from './components/BarChart';
+import data from './data/BarChartData.json'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
+        <div style={{height: 500}}>
+          <charts.BarChart data={data} />
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
